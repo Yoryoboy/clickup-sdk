54d01284e41268e05ff1490cfaf005d217999839
Jorge <93jads@gmail.com>
2025-06-04 14:25:17 -0300

    Preparing for npm

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
-import dotenv from "dotenv";
-import ClickUp from "./src/core/ClickUp.js";
+import ClickUp from "./core/ClickUp.js";
 
-dotenv.config();
+export default ClickUp;
 
-const apiKey = process.env.CLICKUP_API_KEY;
-const teamId = "3051792";
-const listId = "901404730264";
-
-const clickUp = new ClickUp(apiKey);
-
-// Example 1: Get tasks from a specific list
-// const tasks = await clickUp.tasks.getTasks({
-//   list_id: listId,
-// });
-
-// Example 2: Get filtered tasks across the workspace
-// Uncomment and modify with your team_id to test
-
-const filteredTasks = await clickUp.tasks.getFilteredTasks({
-  team_id: teamId,
-  list_ids: [listId],
-  include_closed: true,
-  page: 0,
-  custom_fields: [
-    {
-      field_id: "dbfeb583-6f7f-4819-94fc-86f1e6bcaa2f",
-      operator: "=",
-      value: 0,
-    },
-  ],
-});
-
-console.log(filteredTasks.map((t) => t.reduceInfo()));
+export { default as Task } from "./core/Task.js";
+export { default as TaskManager } from "./core/TaskManager.js";
